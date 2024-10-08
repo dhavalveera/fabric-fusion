9016384c9cb33103c3af1f4c71d6efcb494c9a1b
Dhaval Vira <dhavalveera@gmail.com>
2024-10-08 06:44:35 +0530

    added Auth Route

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+// Auth Module
+import { AuthModule } from './auth/auth.module';
+
+// Middleware
 import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -7,9 +7,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // global prefix e.g. verisioning the URL, change v1 to v2 whenever required
+  app.setGlobalPrefix('api/v1');
+
   // Defining Global Middleware, for Route Specific Middleware define it in app.module.ts with forRoutes()
   // app.use(AuthMiddleware);
 
-  await app.listen(7080);
+  const PORT = process.env.port || 7080;
+
+  await app.listen(PORT);
 }
 bootstrap();
